2a8e5029f184c9c2d45e5aabd0abab91f1946719
vkyoungCN <1662666722@qq.com>
2018-08-13 16:54:40 +0800

    18/08/13A+

--- app/src/main/java/com/vkyoungcn/learningtools/myrhythm/AddRhythmActivity.java
+++ app/src/main/java/com/vkyoungcn/learningtools/myrhythm/AddRhythmActivity.java
@@ -1,12 +1,23 @@
 package com.vkyoungcn.learningtools.myrhythm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class AddRhythmActivity extends AppCompatActivity {
+import static com.vkyoungcn.learningtools.myrhythm.models.Rhythm.RHYTHM_TYPE_24;
+import static com.vkyoungcn.learningtools.myrhythm.models.Rhythm.RHYTHM_TYPE_34;
+import static com.vkyoungcn.learningtools.myrhythm.models.Rhythm.RHYTHM_TYPE_38;
+import static com.vkyoungcn.learningtools.myrhythm.models.Rhythm.RHYTHM_TYPE_44;
+import static com.vkyoungcn.learningtools.myrhythm.models.Rhythm.RHYTHM_TYPE_68;
+
+public class AddRhythmActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private int valueOfBeat = 16;
+    private int valueOfSection = 64;
 
     /* 35个控件，其中33个（非edt的）有点击事件*/
     private TextView tv_x0;
@@ -48,15 +59,13 @@ public class AddRhythmActivity extends AppCompatActivity {
 
     private ImageView imv_longCurve;
 
-    private TextView longCurve_remove;
-    private TextView allConfirm;
+    private TextView tv_longCurve_remove;
+    private TextView tv_allConfirm;
 
-    private TextView lastSection;
-    private TextView nextSection;
-    private TextView lastUnit;
-    private TextView nextUnit;
-
-
+    private TextView tv_lastSection;
+    private TextView tv_nextSection;
+    private TextView tv_lastUnit;
+    private TextView tv_nextUnit;
 
 
     @Override
@@ -64,55 +73,132 @@ public class AddRhythmActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_rhythm);
 
-        tv_x0 = findViewById(R.id.tv_x1_ARA);
-        private TextView tv_xb1;
-        private TextView tv_xb2;
-        private TextView tv_xb3;
+        tv_x0 = findViewById(R.id.tv_x0_ARA);
+        tv_xb1 = findViewById(R.id.tv_xb1_ARA);
+        tv_xb2 = findViewById(R.id.tv_xb2_ARA) ;
+        tv_xb3 = findViewById(R.id.tv_xb3_ARA) ;
 
-        private TextView tv_xp;
-        private TextView tv_xpb1;
-        private TextView tv_xpb2;
+        tv_xp = findViewById(R.id.tv_xp_ARA) ;
+        tv_xpb1 = findViewById(R.id.tv_xpb1_ARA) ;
+        tv_xpb2 = findViewById(R.id.tv_xpb2_ARA) ;
 
-        private TextView tv_xl1;
-        private TextView tv_xl2;
-        private TextView tv_xl3;
+        tv_xl1 = findViewById(R.id.tv_xl1_ARA) ;
+        tv_xl2 = findViewById(R.id.tv_xl2_ARA) ;
+        tv_xl3 = findViewById(R.id.tv_xl3_ARA) ;
 
-        private TextView tv_xm;
-        private TextView tv_xm1;
-        private TextView tv_xm2;
-
-        private ImageView imv_x0;
-        private ImageView imv_xb1;
-        private ImageView imv_xb2;
-        private ImageView imv_xb3;
-
-        private ImageView imv_xp;
-        private ImageView imv_xpb1;
-        private ImageView imv_xpb2;
-
-        private ImageView imv_xl1;
-        private ImageView imv_xl2;
-        private ImageView imv_xl3;
-
-        private ImageView imv_xm;
-        private ImageView imv_xm1;
-        private ImageView imv_xm2;
-
-        private EditText edt_xmNum;
-        private EditText edt_longCurveNum;
-
-        private ImageView imv_longCurve;
-
-        private TextView longCurve_remove;
-        private TextView allConfirm;
-
-        private TextView lastSection;
-        private TextView nextSection;
-        private TextView lastUnit;
-        private TextView nextUnit;
+        tv_xm = findViewById(R.id.tv_xm_ARA) ;
+        tv_xm1 = findViewById(R.id.tv_xm1_ARA) ;
+        tv_xm2 = findViewById(R.id.tv_xm2_ARA) ;
 
 
+        imv_x0 = findViewById(R.id.imv_x0_ARA);
+        imv_xb1 = findViewById(R.id.imv_xb1_ARA) ;
+        imv_xb2 = findViewById(R.id.imv_xb2_ARA) ;
+        imv_xb3 = findViewById(R.id.imv_xb3_ARA) ;
+
+        imv_xp = findViewById(R.id.imv_xp_ARA) ;
+        imv_xpb1 = findViewById(R.id.imv_xpb1_ARA) ;
+        imv_xpb2 = findViewById(R.id.imv_xpb2_ARA) ;
+
+        imv_xl1 = findViewById(R.id.imv_xl1_ARA) ;
+        imv_xl2 = findViewById(R.id.imv_xl2_ARA) ;
+        imv_xl3 = findViewById(R.id.imv_xl3_ARA) ;
+
+        imv_xm = findViewById(R.id.imv_xm_ARA) ;
+        imv_xm1 = findViewById(R.id.imv_xm1_ARA) ;
+        imv_xm2 = findViewById(R.id.imv_xm2_ARA) ;
+
+
+        edt_xmNum = findViewById(R.id.edt_xmNum_ARA);
+        edt_longCurveNum = findViewById(R.id.edt_longCurveSpan_ARA);
+
+        imv_longCurve = findViewById(R.id.imv_longCurveEnd_ARA);
+
+        tv_longCurve_remove =findViewById(R.id.tv_longCurveRemove_ARA) ;
+        tv_allConfirm = findViewById(R.id.tv_confirmAddRhythm_ARA);
+
+        tv_lastSection = findViewById(R.id.tv_lastSection_ARA);
+        tv_nextSection = findViewById(R.id.tv_nextSection_ARA);
+        tv_lastUnit=findViewById(R.id.tv_lastUnit_ARA);
+        tv_nextUnit = findViewById(R.id.tv_nextUnit_ARA);
+
+
+        //设监听
+        imv_x0.setOnClickListener(this);
+        imv_xb1.setOnClickListener(this);
+        imv_xb2.setOnClickListener(this);
+        imv_xb3.setOnClickListener(this);
+
+        imv_xp.setOnClickListener(this) ;
+        imv_xpb1.setOnClickListener(this);
+        imv_xpb2.setOnClickListener(this);
+
+        imv_xl1.setOnClickListener(this);
+        imv_xl2.setOnClickListener(this);
+        imv_xl3.setOnClickListener(this);
+
+        imv_xm.setOnClickListener(this);
+        imv_xm1.setOnClickListener(this);
+        imv_xm2.setOnClickListener(this);
+
+//        edt_xmNum.setOnClickListener(this);
+//        edt_longCurveNum.setOnClickListener(this);
+
+        imv_longCurve.setOnClickListener(this);
+
+        tv_longCurve_remove.setOnClickListener(this);
+        tv_allConfirm.setOnClickListener(this);
+
+        tv_lastSection.setOnClickListener(this);
+        tv_nextSection.setOnClickListener(this);
+        tv_lastUnit.setOnClickListener(this);
+        tv_nextUnit.setOnClickListener(this);
+
+        int rhythmType = getIntent().getBundleExtra("BUNDLE").getInt("RHYTHM_TYPE");
+        switch (rhythmType){
+            case RHYTHM_TYPE_24:
+                valueOfSection = 32;
+                //此时beat值==16无需修改
+                break;
+            case RHYTHM_TYPE_34:
+                valueOfSection = 48;
+                break;
+            case RHYTHM_TYPE_44:
+                valueOfSection = 64;
+                break;
+            case RHYTHM_TYPE_38:
+                valueOfSection = 24;
+                valueOfBeat = 8;
+                break;
+            case RHYTHM_TYPE_68:
+                valueOfSection = 48;
+                valueOfBeat = 8;
+                break;
+        }
+
+        tv_x0.setText(String.valueOf(valueOfBeat));
+        tv_xb1.setText(String.valueOf(valueOfBeat/2));
+        tv_xb2.setText(String.valueOf(valueOfBeat/4));
+        tv_xb3.setText(String.valueOf(valueOfBeat/8));
+
+        tv_xp.setText(String.valueOf(valueOfBeat+valueOfBeat/2));
+        tv_xpb1.setText(String.valueOf(valueOfBeat/2+valueOfBeat/4));
+        tv_xpb2.setText(String.valueOf(valueOfBeat/4+valueOfBeat/8));
+
+        tv_xl1.setText(String.valueOf(valueOfBeat));
+        tv_xl2.setText(String.valueOf(valueOfBeat));
+        tv_xl3.setText(String.valueOf(valueOfBeat));
+
+        tv_xm.setText(String.valueOf(valueOfBeat));
+        tv_xm1.setText(String.valueOf(valueOfBeat));
+        tv_xm2.setText(String.valueOf(valueOfBeat));
 
 
     }
+
+
+
+
+
+
 }
